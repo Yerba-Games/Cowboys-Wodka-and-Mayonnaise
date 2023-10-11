1dbf4967c073f9c3b2270f558d81b0c1c3fb40b5
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2023-10-11 19:46:03 +0200

    EL MOVEMENTO

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using NaughtyAttributes;
+using UnityEngine.Windows;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Inputs inputs; //popiêcie do klasy z inputem
+    [SerializeField][Foldout("Settings")] float Speed;
+    private Rigidbody rb;
+    private InputAction move; //odbiera input z akcji move
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        //g³ównie popina input do wartoœci w skrypcie
+        inputs.Player.Enable();
+        move = inputs.Player.Move;
+    }
+    private void OnDisable()
+    {
+        //zwalnianie ramu.exe
+        inputs.Player.Disable();
+    }
+
+    private void Awake()
+    {
+        //Jebaæ rêczne popinaie
+        rb = GetComponent<Rigidbody>();
+        inputs = new Inputs();
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //movement
+        Vector3 moveForce = new Vector3(move.ReadValue<Vector2>().x, 0, move.ReadValue<Vector2>().y);
+        rb.velocity = moveForce * Speed;
+    }
+    //SEX NIE ISTNIEJE
+}
