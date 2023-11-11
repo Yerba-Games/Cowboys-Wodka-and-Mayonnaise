6d1a4c6ca772ca91e8aeab1ba1ae1e9ba1e1e5b1
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2023-11-11 22:51:55 +0100

    stardet doing levelEnd

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Time.timeScale = 0f;
+    }
+}
