435a690eb6e2eda4ee5e41b603653c05aa5b2f3a
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2023-11-14 21:57:54 +0100

    Added LevelEnd (for now it's an menu)

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
 {
+    [SerializeField] string sceneName;
     private void OnTriggerEnter(Collider other)
     {
-        Time.timeScale = 0f;
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
     }
 }
