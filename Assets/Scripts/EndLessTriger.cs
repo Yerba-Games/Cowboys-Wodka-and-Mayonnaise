2f4079acaba9439954e8b920eb1a69a2e6ca69e6
CLEM\konst <konstantytadeusz@gmail.com>
2024-10-22 13:05:08 +0200

    Added Endless mode for fun

---
+++ Assets/Scripts/EndLessTriger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLessTriger : MonoBehaviour
+{
+    public static EndLessTriger instance;
+    public static bool endless;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void IsEndless()
+    {
+        endless = true;
+        SceneManager.LoadScene("Story");
+    }
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,34 +10,49 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] int spawnCount=3;
     [SerializeField] GameObject[] pickUp;
     [SerializeField] TMP_Text waveProgres;
-    int maxSpawns;
+    int maxSpawns,endleesWaveNumber=0;
+    bool isEndless;
 
     private void Awake()
     {
         if (instance == null)
             instance = this;
         maxSpawns = spawnCount;
+        if (EndLessTriger.endless = true)//for endless mode
+        {
+            waveProgres.text="0";
+            isEndless = true;
+            return;
+        }
         waveProgres.text = $@"{spawnCount+  1}/{maxSpawns+1}";
     }
     public static bool SpawnEnemies()
     {
-        if (instance.spawnCount >= 0)
+        if (instance.isEndless=false)
         {
-            instance.spawnCount--;
-            instance.waveProgres.text = $@"{instance.spawnCount+1}/{instance.maxSpawns+1}";
-            instance.Spawn();
-            return true;
+            if (instance.spawnCount >= 0)
+            {
+                instance.spawnCount--;
+                instance.waveProgres.text = $@"{instance.spawnCount + 1}/{instance.maxSpawns + 1}";
+                instance.Spawn();
+                return true;
+            }
+            instance.waveProgres.text = $@"{instance.spawnCount + 1}/{instance.maxSpawns + 1}";
+            GM.ActivateBossFight();
+            return false;
         }
-        instance.waveProgres.text = $@"{instance.spawnCount+1}/{instance.maxSpawns+1}";
-        GM.ActivateBossFight();
-        return false;
+        instance.Spawn();
+        instance.endleesWaveNumber += 1;
+        instance.waveProgres.text =$@"{instance.endleesWaveNumber}";
+        return true;
+
     }
-    private void Spawn()
+        private void Spawn()
     {
         pickUp[Random.Range(pickUp.Length - 1, 0)].SetActive(true);
         foreach (var enemy in enemysToSpawns)
         {
-            for (int i = 0; i < enemy.enemyNumber; i++)
+            for (int i = 0; i < enemy.enemyNumber+endleesWaveNumber; i++)
             {
                 int index = Random.Range(0, spawnPoints.Length);
                 Debug.Log($@"spawnig at index: {index}");
